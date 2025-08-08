2789373d9004dbb4739515de8e97ad2446b3340d
DESKTOP-PRFLN2G\123ma <123manjittoppo@gmail.com>
2025-08-08 16:30:57 +0530

    2nd commit

--- src/main/java/org/example/employees/EmployeeController.java
+++ src/main/java/org/example/employees/EmployeeController.java
@@ -1,4 +1,19 @@
 package org.example.employees;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return service.getAllEmployees();
+    }
 }
